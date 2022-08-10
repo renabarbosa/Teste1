520449ec0a8a37be072354633a9ba935f5e8a671
unknown <renajbn@gmail.com>
2022-08-10 10:33:24 -0300

    primeiro sucesso

---
+++ Teste1_vs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Teste1_vs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
